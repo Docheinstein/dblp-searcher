5e82fca333296dfc88378ac04bd4cabc78facffb
Stefano Dottore <s.dottore@gti.it>
2019-05-25 09:06:07 +0200

    Fixed makepri for path with spaces

--- makepri.sh
+++ makepri.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-BASEDIR=$(dirname "$0")
+BASEDIR=$(dirname "$(realpath "$0")")
 
-qmake -project -nopwd -o - $BASEDIR/src/ > $BASEDIR/DblpSearcher.pri
-grep -v TARGET $BASEDIR/DblpSearcher.pri > $BASEDIR/DblpSearcher.pri.new
-mv $BASEDIR/DblpSearcher.pri.new $BASEDIR/DblpSearcher.pri
+echo "BASEDIR: $BASEDIR"
+
+qmake -project -nopwd -o - "$BASEDIR/src/" > "$BASEDIR/DblpSearcher.pri"
+grep -v TARGET "$BASEDIR/DblpSearcher.pri" > "$BASEDIR/DblpSearcher.pri.new"
+mv "$BASEDIR/DblpSearcher.pri.new" "$BASEDIR/DblpSearcher.pri"
