fdc4d4d04f76d23f3875fd20a7bdebc6587d4b24
Stefano Dottore <s.dottore@gti.it>
2019-05-25 09:08:04 +0200

    Prettified build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 echo "Creating project file (.pri)"
-echo "==========================="
+
 ./makepri.sh
 
-echo "Cleaning up build directory"
 echo "==========================="
+echo "Cleaning up build directory"
+
 
 rm -rf build
 mkdir build
 cd build
 
-echo "Creating Makefile"
 echo "==========================="
+echo "Creating Makefile"
+
 qmake ../DblpSearcher.pro
 
-echo "Going to build project..."
 echo "==========================="
+echo "Going to build project..."
+
 make -j8
